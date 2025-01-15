2a8502bc640cb350cac42b23ec65632ba5ef7ff6
theMungai <mungaithesimon@gmail.com>
2025-01-15 10:36:56 +0300

    PlayerMove

--- script.js
+++ script.js
@@ -29,7 +29,62 @@ function displayImages(images){
     });
 }
 
+function playerGame(playerMove){
+    const computerMove = randomMove()
 
+    let results = ""
+    if(playerMove === "Rock"){
+        if(computerMove === "Rock")
+        {
+            results = "Tie"
+        }
+
+        else if(computerMove === "Paper")
+        {
+            results = "Computer Wins"
+        }
+        else if(computerMove === "Scissors")
+        {
+            results = "You win"
+        }
+    }
+    
+    else if(playerMove === "Paper")
+    {
+        if(computerMove === "Rock")
+        {
+            results = "You win"
+        }
+
+        else if(computerMove === "Paper")
+        {
+            results = "Tie"
+        }
+
+        else if(computerMove === "Scissors")
+        {
+            results = "Computer wins"
+        }
+    }
+    
+    else if(playerMove === "Scissors")
+    {
+        if(computerMove === "Rock")
+        {
+            results = "Computer wins"
+        }
+
+        else if(computerMove === "Paper")
+        {
+            results = "You win"
+        }
+
+        else if(computerMove === "Scissors")
+        {
+            results = "Tie"
+        }
+    }
+}
 
 function randomMove(){
     const randomNumber = Math.random();
